67ade45662c46568749de81815dccce0277a559a
Brian Alejandro Madsen <brianmadsen1992@hotmail.com>
2023-11-18 14:52:33 +0100

    Main just calls wordCounter

--- src/main/java/challange/Main.java
+++ src/main/java/challange/Main.java
@@ -1,15 +1,11 @@
 package challange;
 
-import challange.domain.service.FileWriterService;
 import challange.domain.service.WordCounter;
 
-import java.io.IOException;
-
 public class Main {
-    static FileWriterService f = new FileWriterService();
     private static WordCounter wordCounter = new WordCounter();
 
-    public static void main(String[] args) throws IOException {   // Creating a TreeMap with a custom comparator for case-insensitive sorting
+    public static void main(String[] args) {
         wordCounter.countWords();
     }
 
