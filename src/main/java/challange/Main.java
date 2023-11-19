f6fd625a349b93aea3e7b525e36ed7dd5c09eb67
Brian Alejandro Madsen <brianmadsen1992@hotmail.com>
2023-11-19 15:45:58 +0100

    Clearing files

--- src/main/java/challange/Main.java
+++ src/main/java/challange/Main.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 public class Main {
+    static WordCounterService wordCounterService = new WordCounterService();
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+    //Uncomment line below to run write to files
+    //wordCounterService.countWords();
+    //Uncomment line below to clear files
+    //wordCounterService.cleanOutputFiles();
     }
 
 
